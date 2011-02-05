888fddd8a0dac38a6b7f9148de32dfe90912bdaf
Callum Macrae <callumacrae95@gmail.com>
2011-02-05 18:58:27 +0000

    ... Im a n00b

--- lynx/includes/includes.php
+++ lynx/includes/includes.php
@@ -1,7 +1,7 @@
 <?php
 
+require_once('lynx/includes/config.php');
 require_once('controller.php');
-require_once('config.php');
 require_once('plugin.php');
 require_once(PATH_INDEX . '/config.php');
 
