26efdfbef7ad6283fb25f6db24c18ed11f23e917
Callum Macrae <callumacrae95@gmail.com>
2011-02-23 15:20:05 +0000

    Fixed issue one
    The plugin doesn't have to be loaded by the controller for get_plugin to
    work anymore: if a plugin calls get_plugin and the plugin isn't loaded,
    it loads it into the controller and then sends a reference to the
    plugin.
    
    Also removed the exists method - it was somewhat pointless.

--- lynx/includes/controller.php
+++ lynx/includes/controller.php
@@ -12,7 +12,7 @@ class Controller
 		$this->hooks = new Hooks;
 	}
 
-	function load($module)
+	function load($module, $plugin = false)
 	{
 		if (isset($this->hooks->modules[$module]))
 		{
@@ -39,7 +39,13 @@ class Controller
 		$this->$module = new $module($module);
 
 		$this->hooks->modules[$module] = true;
-		return 1;
+
+		if ($plugin)
+		{
+			$module =& $this->$module;
+			return $module;
+		}
+		return true;
 	}
 
 	function view($path)
@@ -51,10 +57,5 @@ class Controller
 			return false;
 		}
 		return $path;
-	}
-
-	function exists($module)
-	{
-		return (isset($this->modules[$module]) && $this->modules[$module]);
 	}
 }
