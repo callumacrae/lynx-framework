dea0a0834b00efd4a283f98a0ff2e2e35dfae5b2
Callum Macrae <callumacrae95@gmail.com>
2011-02-26 16:36:34 +0000

    Added comments to the cookies class

--- lynx/plugins/cookies/cookies.php
+++ lynx/plugins/cookies/cookies.php
@@ -14,6 +14,14 @@ class Cookies extends Plugin
 		$this->cookies = $_COOKIE;
 	}
 
+	/**
+	 * Returns value ofcookie. NOTE: It will return a different result
+	 * to $_COOKIE['$cookie'], as it changes the value as it is updated,
+	 * therefore if you set a cookie earlier in the script you will
+	 * get the new value, not the old value.
+	 *
+	 * @param string $cookie The name of the cookie
+	 */
 	function __get($cookie)
 	{
 		if (isset($this->cookies[$cookie]))
@@ -23,17 +31,33 @@ class Cookies extends Plugin
 		return null;
 	}
 
+	/**
+	 * Sets a cookie to a new value.
+	 *
+	 * @param string $cookie The name of the cookie to be set
+	 * @param mixed $value The value to set the cookie to
+	 */
 	function __set($cookie, $value)
 	{
 		$this->cookies[$cookie] = $value;
 		return setcookie($cookie, $value, time() + $this->config['d_expire']);
 	}
 
+	/**
+	 * Returns whether the cookie is set
+	 *
+	 * @param string $cookie The name of the cookie to be checked
+	 */
 	function __isset($cookie)
 	{
 		return isset($this->cookies[$cookie]);
 	}
 
+	/**
+	 * Destroys the cookie
+	 *
+	 * @param string $cookie The name of the cookie to be deleted
+	 */
 	function __unset($cookie)
 	{
 		unset($this->cookies[$cookie]);
