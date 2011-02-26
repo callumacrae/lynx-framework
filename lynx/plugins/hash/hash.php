3cd9584802c7034b7f0504f724d476f3aed51d8a
Callum Macrae <callumacrae95@gmail.com>
2011-02-26 17:15:17 +0000

    Added comments to the hash plugin

--- lynx/plugins/hash/hash.php
+++ lynx/plugins/hash/hash.php
@@ -2,6 +2,19 @@
 
 class Hash extends Plugin
 {
+	/**
+	 * Hashes $input by the hash of $s. If $s isn't specifies, use the salt
+	 * specified in the config. The only non optional paramater is $input,
+	 * although it is recommended to provide a salt, too.
+	 *
+	 * If you specify a salt, it will be hashed using the salt from the config.
+	 *
+	 * @param string $input The string to hash
+	 * @param string $s The salt to use (will be hashed, too)
+	 * @param int $s The Iteration count (should be at least 1000)
+	 * @param int $kl The derived key length
+	 * @param string $a The algorithm to use
+	 */
 	public function pbkdf2($input, $s = null, $c = null, $kl = null, $a = null)
 	{
 		$s = is_null($s) ? $this->config['s'] : $this->pbkdf2($s);
@@ -25,6 +38,7 @@ class Hash extends Plugin
 			$dk .= $ib;
 		}
 
+		//base64_encoded because we don't want loads of random special chars ;)
 		return base64_encode(substr($dk, 0, $kl));
 	}
 }
