4dfc2306f8072e08b88bbe996b0451ae8c00084e
Callum Macrae <callumacrae95@gmail.com>
2011-02-28 21:05:21 +0000

    Added a basic template

--- lynx/apps/views/footer.php
+++ lynx/apps/views/footer.php
@@ -1,2 +1,8 @@
+		</div>
+		<footer>
+			&nbsp;
+		</footer>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
+	</div>
 </body>
 </html>
--- lynx/apps/views/header.php
+++ lynx/apps/views/header.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-	<title>Test Title</head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Lynx Framework</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="/favicon.ico">
 </head>
 <body>
+	<div id="container">
+		<header>
+			<h1>Lynx Framework</h1>
+		</header>
+		<div id="main">
